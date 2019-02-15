9632a4e3aa433776b1284e3ca57b64a0a240d6fc
Maheu <cmaheu@dxc.com>
2019-02-15 13:16:53 -0600

    Removed unneeded files

--- test.js
+++ test.js
@@ -20,9 +20,6 @@ test('responds to requests', (t) => {
       t.false(error);
       // Successful response
       t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
     });
   });
 });

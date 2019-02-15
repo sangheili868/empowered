7218948255525e429bb45223c0cef512e4cf54fd
Maheu <cmaheu@dxc.com>
2019-02-15 15:10:49 -0600

    Removed ejs

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(express.static(path.join(__dirname, 'views')))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/pages/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

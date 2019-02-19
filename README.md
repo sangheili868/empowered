8849f6d2a32a91a66db53cd69185012630664cd9
Maheu <cmaheu@dxc.com>
2019-02-19 10:44:58 -0600

    Used create react app

--- README.md
+++
@@ -1,3 +0,0 @@
-# empowered
-website for my homebrew rpg system
-https://drive.google.com/drive/folders/1NSLfgle6cozbaGc-UWQiO6iCd1t7JVw6?usp=sharing
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(express.static(path.join(__dirname, 'views')))
-  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/pages/index.html')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))

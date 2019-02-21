607a954143237baf87c0a31bf976f79d92ead3a8
Maheu <cmaheu@dxc.com>
2019-02-21 15:37:49 -0600

    Reorganized resources on character sheet

--- client/src/components/pages/Character/CharacterSheetStats.js
+++ client/src/components/pages/Character/CharacterSheetStats.js
@@ -1,63 +1,13 @@
 import React, { Component } from 'react'
-import "./characterSheetStats.scss"
-import CharacterSheetStatsResource from "./CharacterSheetStatsResource"
+import CharacterSheetStatsResources from "./CharacterSheetStatsResources"
 import CharacterSheetStatsWeapons from "./CharacterSheetStatsWeapons"
 import CharacterSheetStatsSkills from "./CharacterSheetStatsSkills"
-import hitPointsIcon from "../../../icons/heart.png"
-import armorIcon from "../../../icons/soldier.png"
-import shieldIcon from "../../../icons/shield.png"
-import woundIcon from "../../../icons/bandage.png"
-import tempHPIcon from "../../../icons/circle-plus.png"
-import speedIcon from "../../../icons/boot.png"
 
 class CharacterSheetStats extends Component {
   render () {
     return (
       <div>
-        <div className="stats">
-          <CharacterSheetStatsResource
-            className="stat"
-            title="Hit Points"
-            icon={hitPointsIcon}
-            value={this.props.stats.hitPoints}
-            subtext={"Max: " + this.props.stats.maxHP}
-          />
-          <CharacterSheetStatsResource
-            className="stat"
-            title="Temp. HP"
-            icon={tempHPIcon}
-            value={this.props.stats.tempHP}
-            subtext={"Max: " + this.props.stats.maxTempHP}
-          />
-          <CharacterSheetStatsResource
-            className="stat"
-            title="Wounds"
-            icon={woundIcon}
-            value={this.props.stats.wounds}
-            subtext={"Limit: " + this.props.stats.maxWounds}
-          />
-          <CharacterSheetStatsResource
-            className="stat"
-            title="Armor"
-            icon={armorIcon}
-            value={this.props.stats.armor.rating}
-            subtext={this.props.stats.armor.type}
-          />
-          <CharacterSheetStatsResource
-            className="stat"
-            title="Shield"
-            icon={shieldIcon}
-            value={this.props.stats.shield.rating}
-            subtext={this.props.stats.shield.type}
-          />
-          <CharacterSheetStatsResource
-            className="stat"
-            title="Speed"
-            icon={speedIcon}
-            value={this.props.stats.speed.rating + "ft."}
-            subtext={this.props.stats.speed.type}
-          />
-        </div>
+        <CharacterSheetStatsResources stats={this.props.stats}/>
         <CharacterSheetStatsSkills
           abilityScores={this.props.stats.abilityScores}
           skills={this.props.stats.skills}
---
+++ client/src/components/pages/Character/CharacterSheetStatsResources.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import {
+  stats,
+  stat,
+  title,
+  info,
+  icon,
+  value,
+  subtext
+} from "./CharacterSheetStatsResources.module.scss"
+import hitPointsIcon from "../../../icons/heart.png"
+import armorIcon from "../../../icons/soldier.png"
+import shieldIcon from "../../../icons/shield.png"
+import woundIcon from "../../../icons/bandage.png"
+import tempHPIcon from "../../../icons/circle-plus.png"
+import speedIcon from "../../../icons/boot.png"
+
+class CharacterSheetStatsResource extends Component {
+  render () {
+    return (
+      <div className={stats}>
+        <div className={stat}>
+          <div className={title}>Hit Points</div>
+          <div className={info}>
+            <img className={icon} src={hitPointsIcon}/>
+            <div className={value}>{this.props.stats.hitPoints}</div>
+            <div className={subtext}>Max: {this.props.stats.maxHP}</div>
+          </div>
+        </div>
+        <div className={stat}>
+          <div className={title}>Temp. HP</div>
+          <div className={info}>
+            <img className={icon} src={tempHPIcon}/>
+            <div className={value}>{this.props.stats.tempHP}</div>
+            <div className={subtext}>Max: {this.props.stats.maxTempHP}</div>
+          </div>
+        </div>
+        <div className={stat}>
+          <div className={title}>Wounds</div>
+          <div className={info}>
+            <img className={icon} src={woundIcon}/>
+            <div className={value}>{this.props.stats.wounds}</div>
+            <div className={subtext}>Limit: {this.props.stats.maxWounds}</div>
+          </div>
+        </div>
+        <div className={stat}>
+          <div className={title}>Armor</div>
+          <div className={info}>
+            <img className={icon} src={armorIcon}/>
+            <div className={value}>{this.props.stats.armor.rating}</div>
+            <div className={subtext}>{this.props.stats.armor.type}</div>
+          </div>
+        </div>
+        <div className={stat}>
+          <div className={title}>Shield Bonus</div>
+          <div className={info}>
+            <img className={icon} src={shieldIcon}/>
+            <div className={value}>{this.props.stats.shield.rating}</div>
+            <div className={subtext}>{this.props.stats.shield.type}</div>
+          </div>
+        </div>
+        <div className={stat}>
+          <div className={title}>Speed</div>
+          <div className={info}>
+            <img className={icon} src={speedIcon}/>
+            <div className={value}>{this.props.stats.speed.rating}ft.</div>
+            <div className={subtext}>{this.props.stats.speed.type}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CharacterSheetStatsResource

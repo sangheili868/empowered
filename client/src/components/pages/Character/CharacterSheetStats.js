a5f522b2b75efe6a4fc2a1ee589ce37800b3423b
Maheu <cmaheu@dxc.com>
2019-02-25 15:19:08 -0600

    Use List component for Equipment and Weapons

--- client/src/components/pages/Character/CharacterSheetStats.js
+++ client/src/components/pages/Character/CharacterSheetStats.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import CharacterSheetStatsResources from "./CharacterSheetStatsResources"
-import CharacterSheetStatsWeapons from "./CharacterSheetStatsWeapons"
 import CharacterSheetStatsSkills from "./CharacterSheetStatsSkills"
-import CharacterSheetStatsEquipment from "./CharacterSheetStatsEquipment"
 import CharacterSheetStatsList from './CharacterSheetStatsList'
-import { chain } from 'lodash'
+import { chain, upperFirst } from 'lodash'
 
 class CharacterSheetStats extends Component {
   render () {
@@ -15,8 +13,49 @@ class CharacterSheetStats extends Component {
           abilityScores={this.props.stats.abilityScores}
           skills={this.props.stats.skills}
         />
-        <CharacterSheetStatsWeapons weapons={this.props.stats.weapons}/>
-        <CharacterSheetStatsEquipment equipment={this.props.stats.equipment}/>
+        <CharacterSheetStatsList
+          title="Weapons"
+          items={
+            chain(this.props.stats.weapons)
+            .reduce(
+              (result, weapon) => {
+                result.name.push(weapon.name)
+                result['Hit Bonus'].push(weapon.bonus)
+                result.damage.push(weapon.damage)
+                result.range.push(weapon.range + 'ft')
+                result.notes.push(weapon.tags && weapon.tags.map(upperFirst).join(', '))
+                return result
+              },
+              { name: [], 'Hit Bonus': [], damage: [], range: [], notes: [] }
+            )
+            .value()
+          }
+        />
+        <CharacterSheetStatsList
+          title="Equipment"
+          subtitles={[
+            <div key="gold">Gold: {this.props.stats.equipment.gold}</div>,
+            <div key="carryWeight">
+              {
+                "Carried Weight: " +
+                this.props.stats.equipment.encumberance.current.toFixed(2) +
+                " / " + this.props.stats.equipment.encumberance.limit.toFixed(2)
+              }
+            </div>
+          ]}
+          items={
+            chain(this.props.stats.equipment)
+            .pick(['heavy', 'medium', 'light'])
+            .mapValues(equipmentList => equipmentList.map(equipment => {
+              if (equipment.name && equipment.quantity) {
+                return `${equipment.name} (${equipment.quantity})`
+              } else {
+                return equipment
+              }
+            }))
+            .value()
+          }
+        />
         <CharacterSheetStatsList title="Languages" items={this.props.stats.languages}/>
         <CharacterSheetStatsList
           title="Proficiencies"
--- client/src/components/pages/Character/CharacterSheetStatsList.js
+++ client/src/components/pages/Character/CharacterSheetStatsList.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
-import { section, title, table, column } from './CharacterPage.module.scss'
-
+import { section, title, subtitle, table, column, columnHeader } from './CharacterPage.module.scss'
+import { startCase } from 'lodash'
 class CharacterSheetStatsList extends Component {
   render () {
     return (
       <div className={section}>
         <div className={title}>{this.props.title}</div>
-        <br/>
+        <div className={subtitle}>
+          {this.props.subtitles}
+        </div>
         <div className={table}>
-          <div className={column}>
-            {this.props.items.map((item, index) =>
-              <div key={index}>{item}</div>
-            )}
-          </div>
+          {Array.isArray(this.props.items) ? (
+            <div className={column}>
+              {this.props.items.map((item, index) =>
+                <div key={index}>{item}</div>
+              )}
+            </div>
+          ) : (typeof this.props.items === 'object' &&
+            Object.keys(this.props.items).map(itemKey => 
+            <div key={itemKey} className={column}>
+              <div className={columnHeader}>{startCase(itemKey)}</div>
+              {this.props.items[itemKey].map((item, index) =>
+                <div key={index}>{item}</div>
+              )}
+            </div>
+            )
+          )}
         </div>
       </div>
     )

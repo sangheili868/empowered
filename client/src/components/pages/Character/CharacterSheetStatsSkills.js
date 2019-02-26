44d50ce9135d66496feec9365f2c4c171c3615ca
Maheu <cmaheu@dxc.com>
2019-02-26 13:26:07 -0600

    Cleaned up skills section

--- client/src/components/pages/Character/CharacterSheetStatsSkills.js
+++ client/src/components/pages/Character/CharacterSheetStatsSkills.js
@@ -1,68 +1,54 @@
 import React, { Component } from 'react'
-import { section, title, list, column, spacer } from "./CharacterPage.module.scss"
+import { section, title, table, columnHeader } from "./CharacterPage.module.scss"
+import { startCase, invert } from 'lodash'
+import skills from '../../../gameData/skills.json'
 
 class CharacterSheetStatsSkills extends Component {
+  scoreNames = Object.keys(this.props.abilityScores)
+  topScores = this.scoreNames.slice(0, this.scoreNames.length/2)
+  leftScores = this.scoreNames.slice(this.scoreNames.length/2, this.scoreNames.length)
   render () {
     return (
       <div className={section}>
         <div className={title}>Skills</div>
-        <br/>
-        <div className={list}>
-          <div className={column}>
-            <div className={spacer}> </div>
-            <div>Quick</div>
-            <div>{this.props.abilityScores.quick}</div>
-            <div>Determined</div>
-            <div>{this.props.abilityScores.determined}</div>
-            <div>Social</div>
-            <div>{this.props.abilityScores.social}</div>
-          </div>
-          <div className={column}>
-            <div>Strong</div>
-            <div>{this.props.abilityScores.strong}</div>
-            <div>Athletics</div>
-            <div>{this.props.skills.athletics}</div>
-            <div>Fortitude</div>
-            <div>{this.props.skills.fortitude}</div>
-            <div>Intimidation</div>
-            <div>{this.props.skills.intimidation}</div>
-            <div>Brawn</div>
-            <div>{this.props.skills.brawn}</div>
-          </div>
-          <div className={column}>
-            <div>Aware</div>
-            <div>{this.props.abilityScores.aware}</div>
-            <div>Stealth</div>
-            <div>{this.props.skills.stealth}</div>
-            <div>Investigation</div>
-            <div>{this.props.skills.investigation}</div>
-            <div>Insight</div>
-            <div>{this.props.skills.insight}</div>
-            <div>Perception</div>
-            <div>{this.props.skills.perception}</div>
-          </div>
-          <div className={column}>
-            <div>Smart</div>
-            <div>{this.props.abilityScores.smart}</div>
-            <div>Handiwork</div>
-            <div>{this.props.skills.handiwork}</div>
-            <div>Wisdom</div>
-            <div>{this.props.skills.wisdom}</div>
-            <div>Synergy</div>
-            <div>{this.props.skills.synergy}</div>
-            <div>Knowledge</div>
-            <div>{this.props.skills.knowledge}</div>
-          </div>
-          <div className={column}>
-            <div className={spacer}> </div>
-            <div>Agility</div>
-            <div>{this.props.skills.agility}</div>
-            <div>Willpower</div>
-            <div>{this.props.skills.willpower}</div>
-            <div>Persuasion</div>
-            <div>{this.props.skills.persuasion}</div>
-          </div>
-        </div>
+        <table className={table}>
+          <tr>
+            <td></td>
+            {this.topScores.map(topScore =>
+              <td>
+                <div className={columnHeader}>{startCase(topScore)}</div>
+                <div>{this.props.abilityScores[topScore]}</div>
+              </td>
+            )}
+          </tr>
+          {this.leftScores.map(leftScore =>
+            <tr>
+            <td>
+              <div className={columnHeader}>{startCase(leftScore)}</div>
+              <div>{this.props.abilityScores[leftScore]}</div>
+            </td>
+              {this.topScores.map(topScore =>
+                <td>
+                  <div>{startCase(invert(skills)[[topScore, leftScore]])}</div>
+                  <div>{this.props.skills[invert(skills)[[topScore, leftScore]]]}</div>
+                </td>
+              )}
+              <td>
+                <div>{startCase(invert(skills)[[leftScore, leftScore]])}</div>
+                <div>{this.props.skills[invert(skills)[[leftScore, leftScore]]]}</div>
+              </td>
+            </tr>
+          )}
+          <tr>
+            <td></td>
+            {this.topScores.map(topScore =>
+              <td>
+                <div>{startCase(invert(skills)[[topScore, topScore]])}</div>
+                <div>{this.props.skills[invert(skills)[[topScore, topScore]]]}</div>
+              </td>
+            )}
+          </tr>
+        </table>
       </div>
     )
   }

223e0a42e7da1f352a08dff3db6218a4f2eae850
Maheu <cmaheu@dxc.com>
2019-04-02 16:29:34 -0500

    Set up local db

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient
-const uri = 'mongodb://heroku_156sz8g7:5kg4hras9q3i7jmcv6c242e7v3@ds249035.mlab.com:49035/heroku_156sz8g7'
+const isProduction = process.env.NODE_ENV === 'production'
+const productionURI = 'mongodb://heroku_156sz8g7:5kg4hras9q3i7jmcv6c242e7v3@ds249035.mlab.com:49035/heroku_156sz8g7'
+const localURI = 'mongodb://localhost:27017/empowered'
+const uri = isProduction ? productionURI : localURI
+const database = isProduction ? 'heroku_156sz8g7' : 'empowered'
 
 MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
   if(err) {
@@ -13,7 +17,7 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
     throw err
   }
 
-  const db = client.db('heroku_156sz8g7')
+  const db = client.db(database)
 
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
@@ -25,7 +29,7 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
   app.post('/api/character/update', character.update.bind(this, db))
   app.post('/api/character/delete', character.delete.bind(this, db))
 
-  if (process.env.NODE_ENV === 'production') {
+  if (isProduction) {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')))
     // Handle React routing, return all requests to React app
